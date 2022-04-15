36e65ee424007281bd671fefd7915a2ebff2c88e
dbotti <davide.botti@lastminute.com>
2022-04-15 11:23:07 +0200

    UseEffect basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+    const [value, setValue] = useState(0);
+    useEffect(() => {
+        console.log('Call use effect')
+        if (value > 0) {
+            document.title = `new Messages(${value})`
+        }
+    }, [value]);
+
+    useEffect(() => {
+        console.log('initial render')
+
+    }, []);
+
+    console.log('render component')
+    return (
+        <>
+            <h1>{value}</h1>
+            <button className='btn' onClick={() => setValue(value + 1)}>Increase</button>
+        </>
+    )
 };
 
 export default UseEffectBasics;
