677fb5b86709c97fc922228d48db36cce2807430
dbotti <davide.botti@lastminute.com>
2022-04-15 10:30:04 +0200

    UseState with an object

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+    const [person, setPerson] = useState({
+            name: 'Davide',
+            age: 29,
+            message: 'random message'
+        })
+    const changeMessage = (person) => {
+        //spread operator!!
+        setPerson({...person, message: 'hello world'})
+    }
+
+    return (
+        <>
+            <h3>{person.name}</h3>
+            <h3>{person.age}</h3>
+            <h3>{person.message}</h3>
+            <button className='btn' onClick={() => changeMessage(person)}>Change message</button>
+        </>
+    )
 };
 
 export default UseStateObject;
