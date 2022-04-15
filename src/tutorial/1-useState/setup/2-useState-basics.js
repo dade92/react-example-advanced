9d1d962498a7d9f7544fbfef90970ca2c2678641
dbotti <davide.botti@lastminute.com>
2022-04-15 10:05:46 +0200

    First usage of useState function

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,26 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+    const initialTitle = 'random title'
+    //hook must be inside the component!!!
+    const [text, setText] = useState(initialTitle)
+
+    const handleClick = () => {
+        if(text === initialTitle) {
+            setText('hello world')
+        } else {
+            setText(initialTitle)
+        }
+    }
+
+    return (
+        <React.Fragment>
+            <h1>{text}</h1>
+            <button className='btn' onClick={handleClick}>
+                Change title
+            </button>
+        </React.Fragment>
+    )
 };
 
 export default UseStateBasics;
