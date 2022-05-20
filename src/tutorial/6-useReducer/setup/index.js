071c18ccc76b47311c869657a3661b4618ef304a
dbotti <davide.botti@lastminute.com>
2022-05-20 11:56:59 +0200

    Use reducer first part

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 // reducer function
+import {data} from "../../../data";
+import Modal from "./Modal";
 
 const Index = () => {
-    return <h2>useReducer</h2>;
+    const [name, setName] = useState('');
+    const [people, setPeople] = useState(data);
+    const [showModal, setShowModal] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (name) {
+            setShowModal(true);
+            setPeople([...people, {id: new Date().getTime().toString(), name}])
+            setName('')
+        } else {
+            setShowModal(true);
+        }
+
+    }
+
+    return (
+        <>
+            {showModal && <Modal/>}
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+                    <button type='submit'>Add</button>
+                </div>
+            </form>
+            {people.map((person) => {
+                return <div key={person.id}>
+                    <h4>{person.name}</h4>
+                </div>
+            })}
+        </>
+    )
 };
 
 export default Index;
