161d42c70bad26c2e36d5da469a265562b0dcfb3
dbotti <davide.botti@lastminute.com>
2022-05-27 09:36:34 +0200

    Use reducer completed

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,37 +1,43 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 // reducer function
 import {data} from "../../../data";
 import Modal from "./Modal";
+import {reducer, defaultState} from './reducer'
 
 const Index = () => {
     const [name, setName] = useState('');
-    const [people, setPeople] = useState(data);
-    const [showModal, setShowModal] = useState(false);
+    const [state, dispatch] = useReducer(reducer, defaultState)
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if (name) {
-            setShowModal(true);
-            setPeople([...people, {id: new Date().getTime().toString(), name}])
+            const newItem = {id: new Date().getTime().toString(), name: name}
+            dispatch({type: 'ADD_ITEM', payload: newItem})
             setName('')
         } else {
-            setShowModal(true);
+            dispatch({type: 'NO_VALUE'})
         }
+    }
 
+    const closeModal = () => {
+        dispatch({type: 'CLOSE_MODAL'})
     }
 
     return (
         <>
-            {showModal && <Modal/>}
-            <form onSubmit={handleSubmit}>
+            {state.isModalOpen && <Modal modalContent={state.modalContent} closeModal={closeModal}/>}
+            <form className='form' onSubmit={handleSubmit}>
                 <div>
                     <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
                     <button type='submit'>Add</button>
                 </div>
             </form>
-            {people.map((person) => {
-                return <div key={person.id}>
+            {state.people.map((person) => {
+                return <div key={person.id} className='item'>
                     <h4>{person.name}</h4>
+                    <button onClick={() => dispatch(
+                        {type: 'REMOVE_ITEM', payload: person.id}
+                    )}>Remove item</button>
                 </div>
             })}
         </>
---
+++ src/tutorial/6-useReducer/setup/reducer.js
@@ -0,0 +1,39 @@
+export const defaultState = {
+    people: [],
+    isModalOpen: false,
+    modalContent: 'hello world',
+}
+
+export const reducer = (state, action) => {
+    if (action.type === 'ADD_ITEM') {
+        const newPeople = [...state.people, action.payload]
+        return {
+            ...state,
+            people: newPeople,
+            isModalOpen: true,
+            modalContent: 'item added'
+        }
+    }
+    if (action.type === 'NO_VALUE') {
+        return {
+            ...state,
+            isModalOpen: true,
+            modalContent: 'No value added'
+        }
+    }
+    if (action.type === 'CLOSE_MODAL') {
+        return {
+            ...state,
+            isModalOpen: false,
+        }
+    }
+    if (action.type === 'REMOVE_ITEM') {
+        const newPeople = state.people.filter((p)=> p.id !== action.payload)
+        return {
+            ...state,
+            people: newPeople,
+            isModalOpen: true,
+            modalContent: 'Item removed'
+        }
+    }
+}
