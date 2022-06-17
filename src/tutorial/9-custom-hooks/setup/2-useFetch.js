923e0f4b11539fb00ab020356d211cd25a7c94e3
dbotti <davide.botti@lastminute.com>
2022-06-17 09:57:23 +0200

    Custom hooks

--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,2 +1,18 @@
-export const useFetch = () => {
+import {useEffect, useState} from "react";
+
+export const useFetch = (url) => {
+    const [loading, setLoading] = useState(true)
+    const [products, setProducts] = useState([])
+
+    const getProducts = async () => {
+        const response = await fetch(url)
+        const products = await response.json()
+        setProducts(products)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        getProducts()
+    }, [url])
+    return {loading, products}
 };
