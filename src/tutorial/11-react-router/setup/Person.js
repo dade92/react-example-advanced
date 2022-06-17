edc97d7836136e1c63c46bad6a7491305a6bbc40
dbotti <davide.botti@lastminute.com>
2022-06-17 12:06:55 +0200

    React router

--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Link, useParams} from "react-router-dom";
+import {data} from "../../../data";
 
 const Person = () => {
+    const [name, setName] = useState('default')
+    const {id} = useParams();
+
+    useEffect(()=>{
+        const newPerson = data.find((p)=>p.id===parseInt(id));
+        setName(newPerson.name);
+    }, [])
+
     return (
         <div>
-            <h2>person</h2>
+            <h2>{name}</h2>
+            <Link to={'/people'}>Back to people</Link>
         </div>
     );
 };
--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import People from "./People";
+import Error from "./Error";
+import Navbar from "./Navbar";
+import Person from "./Person";
 // react router
 // pages
 // navbar
 
 const ReactRouterSetup = () => {
-    return <h2>react router</h2>;
+    return <BrowserRouter>
+        <Navbar/>
+        <Switch>
+            <Route exact path={"/"}>
+                <Home/>
+            </Route>
+            <Route path={"/about"}>
+                <About/>
+            </Route>
+            <Route path={"/people"}>
+                <People/>
+            </Route>
+            <Route path={'/person/:id'} children={<Person/>}/>
+            <Route path={"*"}>
+                <Error/>
+            </Route>
+        </Switch>
+    </BrowserRouter>;
 };
 
 export default ReactRouterSetup;
